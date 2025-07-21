82141e95d8e92a947997ffc8d4aff678151b4a94
marcuslj01 <marcus.jakobsen12@gmail.com>
2025-07-21 19:11:30 +0200

    style: styling on login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -32,14 +32,14 @@ export default function LoginPage() {
   }
 
   return (
-    <div className="flex min-h-screen flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8 bg-white">
+    <div className="flex min-h-screen flex-1 flex-col justify-center items-center py-12 sm:px-6 lg:px-8 bg-white">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <h2 className="mt-6 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
           Sign in to your account
         </h2>
       </div>
 
-      <div className="sm:mx-auto sm:w-full sm:max-w-[480px]">
+      <div className="sm:mx-auto sm:w-full sm:max-w-[480px] max-w-sm">
         <div className="bg-white px-6 py-6 shadow-sm sm:rounded-lg sm:px-12">
           {/* <form action="#" method="POST" className="space-y-6">
             <div>
