f07905927b71a8ad643f7a4409de2ac98682cfa0
marcuslj01 <marcus.jakobsen12@gmail.com>
2025-07-22 22:14:16 +0200

    feat: wrapped products page in adminguard

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -3,19 +3,22 @@
 import React, { useState } from "react";
 import Button from "@/components/ui/Button";
 import UploadModal from "@/components/ui/UploadModal";
+import AdminGuard from "@/components/security/AdminGuard";
 
 function Products() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="h-screen text-white flex items-center justify-center">
-      <Button
-        text="Add Product +"
-        style="primary"
-        onClick={() => setIsOpen(true)}
-      />
-      {isOpen && <UploadModal onClose={() => setIsOpen(false)} />}
-    </div>
+    <AdminGuard>
+      <div className="h-screen text-white flex items-center justify-center">
+        <Button
+          text="Add Product +"
+          style="primary"
+          onClick={() => setIsOpen(true)}
+        />
+        {isOpen && <UploadModal onClose={() => setIsOpen(false)} />}
+      </div>
+    </AdminGuard>
   );
 }
 
