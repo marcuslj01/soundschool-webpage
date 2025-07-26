aeffd4691a17ada65edcfc4b01a173a09a8361aa
marcuslj01 <marcus.jakobsen12@gmail.com>
2025-07-26 23:11:37 +0200

    feat: implement badge on owned packs

--- src/components/ui/PackGrid.tsx
+++ src/components/ui/PackGrid.tsx
@@ -1,17 +1,43 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import { Pack } from "@/lib/types/pack";
 import PackCard from "./PackCard";
+import { OwnedFile } from "@/lib/types/ownedFile";
+import { getOwnedFiles } from "@/lib/firestore/user";
+import { useAuth } from "@/contexts/AuthContext";
 
 interface PackGridProps {
   products: Pack[];
 }
 
-async function PackGrid({ products }: PackGridProps) {
+function PackGrid({ products }: PackGridProps) {
+  const { user } = useAuth();
+  const [ownedFiles, setOwnedFiles] = useState<OwnedFile[]>([]);
+
+  useEffect(() => {
+    if (user) {
+      const fetchOwnedFiles = async () => {
+        const existingOwnedFiles = await getOwnedFiles(user.uid);
+        setOwnedFiles(existingOwnedFiles);
+      };
+      fetchOwnedFiles();
+    }
+  }, [user]);
   return (
     <div className="mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl lg:px-8 flex flex-col gap-4 lg:flex-row">
       {products.map(
         (product) =>
-          !product.hidden && <PackCard key={product.id} product={product} />
+          !product.hidden && (
+            <PackCard
+              key={product.id}
+              product={product}
+              isOwned={ownedFiles.some(
+                (ownedFile) =>
+                  ownedFile.id === product.id && ownedFile.type === "pack"
+              )}
+            />
+          )
       )}
     </div>
   );
