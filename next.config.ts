b50a70baee6f0142a8d0920fb8f33b1eed0de07d
marcuslj01 <marcus.jakobsen12@gmail.com>
2025-08-01 23:48:58 +0200

    feat: added img.youtube.com as allowed image type

--- next.config.ts
+++ next.config.ts
@@ -33,10 +33,16 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
-      
+
       {
         protocol: 'https',
         hostname: 'i.ytimg.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.youtube.com',
         port: '',
         pathname: '/**',
       },
