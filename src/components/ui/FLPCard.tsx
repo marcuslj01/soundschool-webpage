4f553880f7acf38d8189b7bdded0ce0751c04627
marcuslj01 <marcus.jakobsen12@gmail.com>
2025-08-02 00:09:08 +0200

    feat: fixed height on flp title

--- src/components/ui/FLPCard.tsx
+++ src/components/ui/FLPCard.tsx
@@ -10,6 +10,16 @@ interface FLPCardProps {
 }
 
 function FLPCard({ flp, isOwned }: FLPCardProps) {
+  function checkIsNew(date: string) {
+    const today = new Date();
+    const dateObj = new Date(date);
+    const diffTime = Math.abs(today.getTime() - dateObj.getTime());
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+    return diffDays < 3; // If pack is less than 3 days old, it's new
+  }
+
+  const isNew = checkIsNew(flp.created_at.toISOString());
+
   return (
     <div className="w-full bg-gray-900 rounded-lg hover:scale-105 transition-all duration-300">
       <div className="mb-2">
@@ -22,15 +32,25 @@ function FLPCard({ flp, isOwned }: FLPCardProps) {
             className="w-full h-fit object-contain"
           />
         </Link>
-        <div className="flex flex-row justify-between px-4 pt-4">
-          <h3 className="text-gray-200 text-lg font-bold">{flp.name}</h3>
-          <div>{isOwned && <Badge text="OWNED" style="indigo" />}</div>
+        <div className="flex flex-row justify-between px-3 pt-4 h-[80px]">
+          <h3 className="text-gray-200 font-bold line-clamp-2 text-md sm:text-lg">
+            {flp.name}
+          </h3>
+          <div className="px-2">
+            {isOwned ? (
+              <Badge text="OWNED" style="indigo" />
+            ) : flp.is_discounted ? (
+              <Badge text="SALE" style="green" />
+            ) : isNew ? (
+              <Badge text="NEW" style="yellow" />
+            ) : (
+              <Badge text="FLP" style="gray" />
+            )}
+          </div>
         </div>
-        <p className="text-sm font-bold px-4 text-gray-400">
-          {flp.description}
-        </p>
+        <p className="text-sm font-bold px-3 text-gray-400 ">FLP</p>
 
-        <div className="flex flex-row justify-between p-4">
+        <div className="flex flex-row justify-between p-3 ">
           <div className="flex flex-row gap-2 max-w-3/4">
             {flp.tags?.map((tag) => (
               <Badge key={tag} text={tag} style="blue" />
