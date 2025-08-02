bfe3044826cd50a597a90a237602678591cb9be0
marcuslj01 <marcus.jakobsen12@gmail.com>
2025-08-02 21:32:40 +0200

    feat: separate tutorials page

---
+++ src/app/tutorials/page.tsx
@@ -0,0 +1,23 @@
+import FLPGrid from "@/components/ui/FLPGrid";
+import React from "react";
+import { getFLPs } from "@/lib/firestore/flp";
+import Link from "next/link";
+
+export default async function Tutorials() {
+  const flps = await getFLPs();
+  const visibleFlps = flps.filter((flp) => !flp.hidden);
+
+  return (
+    <div className="flex flex-col gap-4 w-full items-center text-white min-h-screen justify-center mt-16">
+      <h2 className="text-2xl font-bold text-white sm:text-4xl">
+        All our tutorials with FLPs!
+      </h2>
+      <Link href="https://www.youtube.com/@Soundschool18" target="_blank">
+        <h3 className="text-gray-400 text-md hover:text-white transition-all duration-300">
+          Check out our YouTube channel!
+        </h3>
+      </Link>
+      <FLPGrid flps={visibleFlps} />
+    </div>
+  );
+}
