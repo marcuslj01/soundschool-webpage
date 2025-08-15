1f9aadbbbc8906a287ea8b758c1372705838a2de
marcuslj01 <marcus.jakobsen12@gmail.com>
2025-08-15 23:17:30 +0200

    fix: padding to avoid overflowing

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -101,7 +101,7 @@ function Hero(props: HeroProps) {
             height={1000}
             className="w-full h-full object-contain"
           />
-          <div className="flex flex-row items-center justify-center gap-4 mt-8 pb-8">
+          <div className="flex flex-row items-center justify-center gap-4 mt-8 pb-8 sm:pb-0">
             <Link
               className="bg-primary text-white px-4 py-2 rounded-md hover:bg-primary/80 transition-all duration-300"
               href={props.packLink}
