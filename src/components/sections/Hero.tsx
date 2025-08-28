538daf577c8b3dfe82776fafc750191553d2e7f2
marcuslj01 <marcus.jakobsen12@gmail.com>
2025-08-28 20:45:11 +0200

    fix: image size

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -70,7 +70,7 @@ function Hero(props: HeroProps) {
 
         {/* Product image with animation */}
         <motion.div
-          className="md:block hidden w-1/3 min-w-[300px]"
+          className="md:block hidden md:w-1/3 min-w-[300px] lg:w-1/5"
           initial={{ opacity: 0, x: 50 }}
           animate={{ opacity: 1, x: 0 }}
           transition={{ duration: 0.6, delay: 0.2 }}
